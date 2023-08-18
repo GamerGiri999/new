91ed46cce56fded7523a3ee79085e23e288acbbd
Remon Islam <93038033+GamerGiri999@users.noreply.github.com>
2023-08-18 10:34:13 +0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/GamerGiri/Shortner-Converter-Bot-V2.git /Shortner-Converter-Bot-V2
-cd /Shortner-Converter-Bot-V2
+git clone https://github.com/GamerGiri/new.git /new
+cd /new
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
